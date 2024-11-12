db742858d504a2705bffa32cb488d52b61dc23be
Mohammad Albay <MohammadAlbay99@gmail.com>
2024-11-12 11:05:49 +0200

    moved to laravel

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once __DIR__.'/config/Config.php';
-use Config\Config;
-Config::setPath(__DIR__);
-require_once __DIR__ . '/vendor/autoload.php';
-include_once __DIR__.'/app/routes/web.php';
-//echo "La Porta Di Roma AAAA";
-
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitc27ec22ccc200d7c51d1d9b754f315bf::getLoader();
+return ComposerAutoloaderInit589cc0f65c3aaa7d4e9024d545bc1974::getLoader();
